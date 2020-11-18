1726bdfefe960b43c752373e21797ae992f9d995
Isaac Nam <ikn208@nyu.edu>
2020-11-18 15:24:07 -0500

    learning ts

---
+++ main.ts
@@ -0,0 +1,27 @@
+export {}
+
+let message = 'Welcome Back';
+console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'Codevolution';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Isaac';
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
