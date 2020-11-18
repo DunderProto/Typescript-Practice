66078ec4ed77c58ce49397a6c370925f1e178c27
Isaac Nam <ikn208@nyu.edu>
2020-11-18 15:57:46 -0500

    typescript classes

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = 'Welcome Back';
 console.log(message);
@@ -30,7 +43,7 @@ console.log(c);
 var randomValue = 10;
 randomValue = true;
 randomValue = 'Isaac';
-var myVariable = 10;
+var myVariable = 'some';
 function hasName(obj) {
     return !!obj &&
         typeof obj === "object" &&
@@ -50,3 +63,46 @@ var b = 20;
 var multiType;
 multiType = 20;
 multiType = true;
+// optional parameter with a ? 
+// they must always be after the required parameters
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce'
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Isaac');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -37,7 +37,7 @@ let randomValue: any = 10;
 randomValue = true;
 randomValue = 'Isaac';
 
-let myVariable: unknown = 10;
+let myVariable: unknown = 'some';
 
 function hasName(obj: any): obj is {name: string} {
     return !!obj &&
@@ -64,3 +64,60 @@ let b = 20;
 let multiType: number | boolean;
 multiType = 20;
 multiType = true;
+
+        // optional parameter with a ? 
+        // they must always be after the required parameters
+function add(num1: number, num2: number = 10): number {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+
+add(5, 10);
+
+interface Person {
+    firstName: string;
+    lastName?: string;
+}
+
+function fullName(person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p = {
+    firstName: 'Bruce'
+};
+
+fullName(p);
+
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`);
+    }
+}
+
+let emp1 = new Employee('Isaac');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+
+    delegateWork() {
+        console.log(`Manager delegating tasks`)
+    }
+}
+
+let m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
